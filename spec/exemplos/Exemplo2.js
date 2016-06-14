212c2a2f7a4386b4c80fb7324fb28e489c213ac6
Leandro dos Reis Oliveira <lroliveira@fcl.com.br>
2016-06-14 09:31:08 -0300

    Exemplo inicial

--- spec/exemplos/Exemplo2.js
+++
@@ -1,9 +0,0 @@
-describe("Exemplo 2", function() {
-
-  var exemplo3 = require('../../lib/Exemplo3');
-
-  it("executando teste2 ", function() {
-     expect(exemplo2()).toEqual(true);
-  });
-
-});
